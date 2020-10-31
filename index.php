da1a395627d45d0c11591650ed38536f0996aae6
muhammadzidane <muhammmadzidanealsaadawi@gmail.com>
2020-10-31 19:33:30 +0700

    Penambahan index

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Belajar PHP</title>
+</head>
+<body>
+	<table border="1">
+		<thead>
+			<tr>
+				<td>No</td>
+				<td>Foto</td>
+				<td>Nama Siswa</td>
+			</tr>
+		</thead>
+		<thead>
+			<tr>
+				<td>1</td>
+				<td>Test</td>
+				<td>Restu Dwi Cahyo</td>
+			</tr>
+		</thead>
+	</table>
+</body>
+</html>
